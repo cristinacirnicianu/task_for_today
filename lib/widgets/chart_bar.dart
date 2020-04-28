1028f8898e70e64f1a9dd1bb956d5fe5f6b91ab7
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-04-28 11:19:26 +0300

    create bar of chart

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingHours;
+  final double spendingProcentOfTotal;
+
+  ChartBar(this.label, this.spendingHours, this.spendingProcentOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('h${spendingHours.toStringAsFixed(0)}'),
+        SizedBox(height: 4,),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.grey, width: 1.0),
+                color: Color.fromRGBO(220, 220, 220, 1),
+                borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
+        ),
+        FractionallySizedBox(heightFactor: spendingProcentOfTotal,
+          child: Container(
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(10),),
+          ),
+        ),
+        SizedBox(height: 4,),
+        Text(label)
+      ],
+    );
+  }
+}
