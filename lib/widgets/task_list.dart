217afde63acf0ae257ae6ef706cf7a744421b6de
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-04-29 11:32:12 +0300

    modify task_list by replacing card with ListTile

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,62 +1,60 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../models/task_model.dart';
 import 'package:intl/intl.dart';
 
 class TaskList extends StatelessWidget {
   final List<Task> tasks;
+
   TaskList(this.tasks);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
       height: 300,
-      child: tasks.isEmpty ? Column(
-        children: [
-          Text('No tasks added yet!', style: Theme.of(context).textTheme.headline6,),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            height: 200,
-              child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover,)),
-        ],
-      ) : ListView.builder(
-        itemBuilder: (context, index) {
-          return Card(
-            child: Row(
+      child: tasks.isEmpty
+          ? Column(
               children: [
-                Container(
-                  child: Text(
-                    tasks[index].hourTask.toStringAsFixed(2) + 'h',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                        color: Theme.of(context).primaryColor,
-                    ),),
-                  margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Theme.of(context).primaryColor, width: 2),
-                  ),
-                  padding: EdgeInsets.all(10),
+                Text(
+                  'No tasks added yet!',
+                  style: Theme.of(context).textTheme.headline6,
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(tasks[index].name,
-                      style: Theme.of(context).textTheme.headline6),
-                    Text(DateFormat.yMMMd().format(tasks[index].dateTime),
-                      style: TextStyle(color: Theme.of(context).primaryColor),),
-                  ],
-                )
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    )),
               ],
+            )
+          : ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  elevation: 5,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: FittedBox(
+                            child: Text(
+                          tasks[index].hourTask.toStringAsFixed(2) + 'h',
+                        )),
+                      ),
+                    ),
+                    title: Text(tasks[index].name,
+                        style: Theme.of(context).textTheme.headline6),
+                    subtitle: Text(DateFormat.yMMMd().format(tasks[index].dateTime),
+                      style:TextStyle(color:Theme.of(context).primaryColor),),
+                  ),
+                );
+              },
+              itemCount: tasks.length,
             ),
-          );
-        },
-        itemCount: tasks.length,
-        ),
     );
   }
 }
