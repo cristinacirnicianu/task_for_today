19862e2f1c39ee6d5f2c73e78c69815c59a9c803
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-04-30 12:03:33 +0300

    clean code from repeating MediaQuery object

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-   final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+   final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Task for today'),
       actions: [
@@ -98,9 +99,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final tkListWidge = Container(
-        height: (MediaQuery.of(context).size.height -
+        height: (mediaQuery.size.height -
             appBar.preferredSize.height -
-            MediaQuery.of(context).padding.top) *
+            mediaQuery.padding.top) *
             0.7,
         child: TaskList(_userTasks, _deleteTask));
     return Scaffold(
@@ -125,17 +126,17 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
             ),
             if(!isLandscape) Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                     appBar.preferredSize.height -
-                    MediaQuery.of(context).padding.top) *
+                    mediaQuery.padding.top) *
                     0.3,
                 child: Chart(_recentTasks)),
             if(!isLandscape) tkListWidge,
 
             if(isLandscape) _showChart ? Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                         appBar.preferredSize.height -
-                        MediaQuery.of(context).padding.top) *
+                    mediaQuery.padding.top) *
                     0.7,
                 child: Chart(_recentTasks)) :  tkListWidge ,
           ],
