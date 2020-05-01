5f7f71ce6b762a5e4f94370a561b60f205dbebfe
Cristina Cirnicianu <cristina.cirnicianu@gmail.com>
2020-05-01 09:26:54 +0300

    check app lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -42,9 +42,27 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
   final List<Task> _userTasks = [];
   bool _showChart = false;
+
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print(state);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
 
   List<Task> get _recentTasks {
     return _userTasks.where((tk) {
@@ -85,87 +103,115 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
- List<Widget> _buildLandscapeContent(MediaQueryData mediaQuery, AppBar appBar, Widget tkListWidge) {
-    return [Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text('Show Chart', style: Theme.of(context).textTheme.bodyText1,),
-        Switch.adaptive(
-          activeColor: Theme.of(context).accentColor,
-          value: _showChart,
-          onChanged: (val) {
-            setState(() {
-              _showChart = val;
-            });
-          },
-        )
-      ],
-    ),
-      _showChart ? Container(
-          height: (mediaQuery.size.height -
-              appBar.preferredSize.height -
-              mediaQuery.padding.top) *
-              0.7,
-          child: Chart(_recentTasks))
-    : tkListWidge];
+  List<Widget> _buildLandscapeContent(
+      MediaQueryData mediaQuery, AppBar appBar, Widget tkListWidge) {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Show Chart',
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
+          Switch.adaptive(
+            activeColor: Theme.of(context).accentColor,
+            value: _showChart,
+            onChanged: (val) {
+              setState(() {
+                _showChart = val;
+              });
+            },
+          )
+        ],
+      ),
+      _showChart
+          ? Container(
+              height: (mediaQuery.size.height -
+                      appBar.preferredSize.height -
+                      mediaQuery.padding.top) *
+                  0.7,
+              child: Chart(_recentTasks))
+          : tkListWidge
+    ];
   }
-  List<Widget> _buildPortraitContent(MediaQueryData mediaQuery, AppBar appBar, Widget tkListWidge) {
-    return [Container(
-        height: (mediaQuery.size.height -  appBar.preferredSize.height -
-            mediaQuery.padding.top) *
-            0.3,
-        child: Chart(_recentTasks)),
-      tkListWidge];
+
+  List<Widget> _buildPortraitContent(
+      MediaQueryData mediaQuery, AppBar appBar, Widget tkListWidge) {
+    return [
+      Container(
+          height: (mediaQuery.size.height -
+                  appBar.preferredSize.height -
+                  mediaQuery.padding.top) *
+              0.3,
+          child: Chart(_recentTasks)),
+      tkListWidge
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-   final isLandscape = mediaQuery.orientation == Orientation.landscape;
-    final PreferredSizeWidget appBar = Platform.isIOS ? CupertinoNavigationBar(
-      middle: Text('My daily tasks'),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(CupertinoIcons.add),
-          GestureDetector(onTap: () => _startAddNewTask(context),)
-        ],
-      ),
-    ) : AppBar(
-      title: Text('Task for today'),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => _startAddNewTask(context),
-        )
-      ],
-    );
-     final tkListWidge = Container(
+    final isLandscape = mediaQuery.orientation == Orientation.landscape;
+    final PreferredSizeWidget appBar = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text('My daily tasks'),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(CupertinoIcons.add),
+                GestureDetector(
+                  onTap: () => _startAddNewTask(context),
+                )
+              ],
+            ),
+          )
+        : AppBar(
+            title: Text('Task for today'),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () => _startAddNewTask(context),
+              )
+            ],
+          );
+    final tkListWidge = Container(
         height: (mediaQuery.size.height -
-            appBar.preferredSize.height -
-            mediaQuery.padding.top) *
+                appBar.preferredSize.height -
+                mediaQuery.padding.top) *
             0.7,
         child: TaskList(_userTasks, _deleteTask));
 
-    final pageBody = SafeArea(child: SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          if(isLandscape) ... _buildLandscapeContent(mediaQuery, appBar, tkListWidge),
-          if(!isLandscape)... _buildPortraitContent(mediaQuery, appBar, tkListWidge),
-        ],
-      ),
-    ), );
-
-    return Platform.isIOS ? CupertinoPageScaffold(child:  pageBody, navigationBar: appBar,) : Scaffold(
-      appBar: appBar,
-      body: pageBody,
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Platform.isIOS ? Container() : FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () => _startAddNewTask(context),
+    final pageBody = SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            if (isLandscape)
+              ..._buildLandscapeContent(mediaQuery, appBar, tkListWidge),
+            if (!isLandscape)
+              ..._buildPortraitContent(mediaQuery, appBar, tkListWidge),
+          ],
+        ),
       ),
     );
+
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: pageBody,
+            navigationBar: appBar,
+          )
+        : Scaffold(
+            appBar: appBar,
+            body: pageBody,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () => _startAddNewTask(context),
+                  ),
+          );
   }
 }
